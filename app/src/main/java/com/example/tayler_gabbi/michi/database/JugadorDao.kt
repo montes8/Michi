450a43cd647545965d2c959430beb066dd72dca8
montes8 <emontesinos812@gmail.com>
2018-12-06 07:43:30 -0500

    creando interfaz con los dao

---
+++ app/src/main/java/com/example/tayler_gabbi/michi/database/JugadorDao.kt
@@ -0,0 +1,21 @@
+package com.example.tayler_gabbi.michi.database
+
+import android.arch.persistence.room.*
+import com.example.tayler_gabbi.michi.database.model.Jugador
+
+
+@Dao
+interface JugadorDao {
+
+    @Query("select * from Jugador")
+    fun listarJugador(): List<Jugador>
+
+    @Insert
+    fun insert(jugador: Jugador): Long
+
+    @Delete
+    fun deleteJugador(jugador: Jugador):Int
+
+    @Update
+    fun updateJugador(jugador: Jugador)
+}
