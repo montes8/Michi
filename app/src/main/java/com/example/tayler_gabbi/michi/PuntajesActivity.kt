1a088b891791dcdfb9672c6ea2c083a5b868cc58
montes8 <emontesinos812@gmail.com>
2018-12-08 09:06:17 -0500

    agreando toolbar al activityPuntaje

--- app/src/main/java/com/example/tayler_gabbi/michi/PuntajesActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/michi/PuntajesActivity.kt
@@ -2,16 +2,19 @@ package com.example.tayler_gabbi.michi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_puntajes.*
 
 class PuntajesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puntajes)
+
+        ajusteToolbarPuntaje()
     }
 
-    private fun ajusteToolbarHome() {
-        setSupportActionBar(homeToolbar)
+    private fun ajusteToolbarPuntaje() {
+        setSupportActionBar(toolbar_puntajes)
         title = "LISTA DE PUNTAJES"
     }
 }
