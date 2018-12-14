19af547fd0543b1e12ce1e6e2f26f557454c07fc
montes8 <emontesinos812@gmail.com>
2018-12-14 09:20:21 -0500

    cargando la lista a la vista

--- app/src/main/java/com/example/tayler_gabbi/michi/PuntajesActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/michi/PuntajesActivity.kt
@@ -2,15 +2,35 @@ package com.example.tayler_gabbi.michi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.widget.LinearLayoutManager
+import com.example.tayler_gabbi.michi.adapter.ListaPuntajesAdapter
 import kotlinx.android.synthetic.main.activity_puntajes.*
 
 class PuntajesActivity : AppCompatActivity() {
+
+    var puntajeAdapter : ListaPuntajesAdapter? = null
+    var handler :Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puntajes)
 
+        puntajeAdapter = ListaPuntajesAdapter()
+
+        my_recyclerview.layoutManager = LinearLayoutManager(this)
+        my_recyclerview.adapter = puntajeAdapter
+
+        Thread {
+            val lista = MichiApplication.dataBase!!.puntajeDao().listarPuntaje()
+            handler.post {
+                puntajeAdapter!!.addList(lista)
+            }
+        }.start()
+
         ajusteToolbarPuntaje()
+
+
     }
 
     private fun ajusteToolbarPuntaje() {
